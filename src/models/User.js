d784ff1504bcf241d08e64103e197c42929eaf94
BenThere <benjaminbirdsall@icloud.com>
2024-07-02 20:50:33 -0600

    updated user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,54 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
-    createdAt: { type: Date, default: Date.now }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['User', 'Admin'],
+    default: 'User',
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
+  favoriteParks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Park',
+  }],
+  recentlyViewedParks: [{
+    park: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Park',
+    },
+    viewedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }],
+  recentlyVisitedParks: [{
+    park: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Park',
+    },
+    visitedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }],
+  unreadConversationsCount: {
+    type: Number,
+    default: 0,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
