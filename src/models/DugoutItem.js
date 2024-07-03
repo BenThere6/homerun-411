485eb43f56ae7f6a479a80bab481dad36a07c470
BenThere <benjaminbirdsall@icloud.com>
2024-07-02 21:10:04 -0600

    update dugout item modle

--- src/models/DugoutItem.js
+++ src/models/DugoutItem.js
@@ -1,11 +1,46 @@
 const mongoose = require('mongoose');
 
-const dugoutItemSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    price: { type: Number, required: true },
-    condition: { type: String, enum: ['New', 'Used'], default: 'Used' },
-    imageUrl: { type: String }
+const dugoutSwapItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  condition: {
+    type: String,
+    enum: ['New', 'Used'],
+    default: 'Used',
+  },
+  imageURL: {
+    type: String,
+    required: true,
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('DugoutItem', dugoutItemSchema);
+const DugoutSwapItem = mongoose.model('DugoutSwapItem', dugoutSwapItemSchema);
+
+module.exports = DugoutSwapItem;
