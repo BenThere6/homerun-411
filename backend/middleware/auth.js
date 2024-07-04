1305a3fb7e8d743a21213fdf6fc1b3604673b75e
BenThere <benjaminbirdsall@icloud.com>
2024-07-04 13:11:17 -0600

    allow all requests on dev server

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,25 +1,31 @@
 const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
-  // 1. Retrieve token from Authorization header
+  // Check if environment is development
+  if (process.env.NODE_ENV === 'development') {
+    // Allow all requests in development mode
+    return next();
+  }
+
+  // Retrieve token from Authorization header
   const token = req.header('Authorization');
 
-  // 2. Check if token is present
+  // Check if token is present
   if (!token) {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
 
   try {
-    // 3. Verify the token using JWT_SECRET from environment variables
+    // Verify the token using JWT_SECRET from environment variables
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // 4. Attach decoded user information to request object
+    // Attach decoded user information to request object
     req.user = decoded.user;
 
-    // 5. Proceed to the next middleware or route handler
+    // Proceed to the next middleware or route handler
     next();
   } catch (err) {
-    // 6. Handle token verification errors
+    // Handle token verification errors
     res.status(401).json({ message: 'Invalid token' });
   }
 };
--- backend/middleware/isAdmin.js
+++ backend/middleware/isAdmin.js
@@ -1,4 +1,10 @@
 const isAdmin = (req, res, next) => {
+    // Check if environment is development
+    if (process.env.NODE_ENV === 'development') {
+        // Allow all requests in development mode
+        return next();
+    }
+    
     if (req.user.role !== 'Admin') {
         return res.status(403).json({ message: 'Access denied. Admin authorization required.' });
     }
