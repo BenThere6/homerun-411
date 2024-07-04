3f191aafc7914cfedfc9b0c0f79b3ae31aec45dd
BenThere <benjaminbirdsall@icloud.com>
2024-07-04 09:25:14 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // Other Vite configurations
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5173', // Your backend server URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
