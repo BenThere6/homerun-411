3f56fc5a5baf31423e54e38903843b67d08a807d
BenThere <benjaminbirdsall@icloud.com>
2024-09-08 12:40:43 -0600

    start app for HR411

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const User = require('./models/User'); // Ensure you have the User model
 const bcrypt = require('bcrypt');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
