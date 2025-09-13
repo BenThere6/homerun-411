52912dfa688cf4ab502528a83d9d5219ee0277c5
BenThere <benjaminbirdsall@icloud.com>
2025-09-13 16:27:32 -0500

    udpate app

--- frontend/homeRun411/babel.config.js
+++ frontend/homeRun411/babel.config.js
@@ -1,14 +1,15 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ['module:react-native-dotenv', {
+// babel.config.js
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
         moduleName: '@env',
         path: '.env',
-        safe: false,
-        allowUndefined: true,
-      }]
-    ]
-  };
+        safe: false,           // set to true only if you use .env.example
+        allowUndefined: true,  // avoids build failing if a var is missing
+      },
+    ],
+  ],
 };
--- frontend/homeRun411/metro.config.js
+++ frontend/homeRun411/metro.config.js
@@ -1,26 +1,11 @@
 // metro.config.js
-// Expo requires extending @expo/metro-config for reliable prod builds.
+const { getDefaultConfig } = require('expo/metro-config');
 
-const { getDefaultConfig } = require('@expo/metro-config');
-const path = require('path');
+const defaultConfig = getDefaultConfig(__dirname);
 
-const projectRoot = __dirname;
-const config = getDefaultConfig(projectRoot);
+// Enable SVGs via react-native-svg-transformer
+defaultConfig.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
+defaultConfig.resolver.sourceExts.push('svg');
 
-// ---- OPTIONAL CUSTOMIZATIONS ----
-// If you use SVGs as React components:
-//   npm i -D react-native-svg-transformer
-//   npm i react-native-svg
-// Then uncomment this block:
-//
-// config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-// config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
-// config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
-
-// If you have extra asset/file extensions, you can push them, e.g.:
-// config.resolver.assetExts = [...config.resolver.assetExts, 'db', 'mp3'];
-
-// If you need to include monorepo/workspaces, map them here (example):
-// config.watchFolders = [path.resolve(projectRoot, '../shared')];
-
-module.exports = config;
+module.exports = defaultConfig;
